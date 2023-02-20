e56b992866909fab840bd891965763e37be33231
Henok Bekele <henokbekele1997@gmail.com>
2023-02-20 12:43:40 -0600

    minimiazed the return statement

--- BinaryAddition.py
+++ BinaryAddition.py
@@ -1,30 +1,6 @@
 def add_binary(a,b):
     sum = a+b
-    sum = bin(sum)
-    sum = sum[2::]
-    return sum
-
-    # sum = a+b
-    # firstBin = ''
-    # arr = []
-    # while(sum!=1):
-    #     if(sum%2!=0):
-    #         #arr.append(1)
-    #         sum = sum - 1
-    #         firstBin = '1'+firstBin
-
-    #     else:
-    #         #arr.append(0)
-    #         firstBin = '0'+firstBin
-    #     sum = sum/2
-
-    # if(sum ==1):
-    #     #arr.append(1)
-    #     firstBin = '1'+firstBin
-    # #arr = arr[::-1]
-    # #return ("".join(str(e) for e in arr))
-
-    # return firstBin
+    return bin(sum)[2::]
 
 print(add_binary(15, 2))
 
