9f3150a1526526fee173fc5b720675ffe5f3729e
Henok Bekele <henokbekele1997@gmail.com>
2023-02-27 22:17:05 -0600

    changes

--- TopKfrequent.py
+++ TopKfrequent.py
@@ -6,9 +6,32 @@ def topKFrequent( nums, k):
             res[x]=res[x] + 1
         else:
             res[x]=1
-        if res[x] >= k:
-            tot.append(x)
     
-    return set(tot)
+    for x in range(k):
+        maxi = max(res,key=lambda x:res[x])
+        tot.append(maxi)
+        res[maxi] = 0
 
-print(topKFrequent([1,2,3,3,4,5,5],2))
+    
+    return tot
+
+def topKFrequentBucketSort(nums, k):
+    count = {}
+    freq = [[] for i in range(len(nums)+1)]
+
+    for n in nums:
+        count[n] = 1 + count.get(n,0)
+    for n, c in count.items():
+        freq[c].append(n)
+
+    res = []
+    for i in range(len(freq)-1,0,-1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
+    
+
+
+print(topKFrequent([1,1,1,2,2,3],2))
+print(topKFrequentBucketSort([1,1,1,2,2,3], 2))
