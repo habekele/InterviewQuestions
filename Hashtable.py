8b0a9ec6d21622a448a80026f0513c519cdf4900
Henok Bekele <henokbekele1997@gmail.com>
2023-03-19 20:29:22 -0500

    Some lil pages i worked on

---
+++ Hashtable.py
@@ -0,0 +1,16 @@
+def merge(a,b,c):
+
+    d = []
+
+    for w in a:
+        d.append(w)
+        print(w)
+    for w in b:
+        d.append(w)
+    for w in c:
+        d.append(w)
+
+    return d
+    
+
+print(merge("Mrs","Tru","Johnson" ))
