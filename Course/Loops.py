701cadb91e3d07f9de7a2586b2401fed1cef00c5
Henok Bekele <henokbekele1997@gmail.com>
2023-03-21 21:15:09 -0500

    some one practice with loops

--- Course/Loops.py
+++ Course/Loops.py
@@ -10,17 +10,43 @@
 # else:
 #     print('done all the work')
 
-my_list = [1,2,3]
-for item in my_list:
-    continue
-    print(item)
+# my_list = [1,2,3]
+# for item in my_list:
+#     continue
+#     print(item)
 
-i = 0
-while i < len(my_list):
-    print(my_list[i])
-    i+=1
+# i = 0
+# while i < len(my_list):
+#     print(my_list[i])
+#     i+=1
 
-while True:
-    response = input('say something: ')
-    if(response=='bye'):
-        break
+# while True:
+#     response = input('say something: ')
+#     if(response=='bye'):
+#         break
+
+
+# picture = [
+#     [0,0,0,1,0,0,0],
+#     [0,0,1,1,1,0,0],
+#     [0,1,1,1,1,1,0],
+#     [1,1,1,1,1,1,1],
+#     [0,0,0,1,0,0,0],
+#     [0,0,0,1,0,0,0],
+
+# ]
+# for image in picture:
+#     for pixel in image:
+#         if pixel:
+#             print('*',end='')
+#         else:
+#             print(' ',end='')
+#     print('')
+
+
+some_list = ['a','b','c','b','d','m','n','n']
+duplicate = []
+for item in some_list:
+    if some_list.count(item) > 1 and item not in duplicate:
+        duplicate.append(item)
+print (duplicate)
