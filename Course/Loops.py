4ab28e7da7026b244d8918c757128e311bc04b17
Henok Bekele <henokbekele1997@gmail.com>
2023-03-23 20:59:51 -0500

    docstrings

--- Course/Loops.py
+++ Course/Loops.py
@@ -1,56 +1,81 @@
-# for i, char in enumerate(list(range(100))):
-#     if char == 50:
-#         print(f"index of 50 is: {i}")
+# # for i, char in enumerate(list(range(100))):
+# #     if char == 50:
+# #         print(f"index of 50 is: {i}")
 
-# i = 0
-# while i < 50:
-#     print(i)
-#     i += 1
+# # i = 0
+# # while i < 50:
+# #     print(i)
+# #     i += 1
     
-# else:
-#     print('done all the work')
+# # else:
+# #     print('done all the work')
 
-# my_list = [1,2,3]
-# for item in my_list:
-#     continue
-#     print(item)
+# # my_list = [1,2,3]
+# # for item in my_list:
+# #     continue
+# #     print(item)
 
-# i = 0
-# while i < len(my_list):
-#     print(my_list[i])
-#     i+=1
+# # i = 0
+# # while i < len(my_list):
+# #     print(my_list[i])
+# #     i+=1
 
-# while True:
-#     response = input('say something: ')
-#     if(response=='bye'):
-#         break
-
-
-# picture = [
-#     [0,0,0,1,0,0,0],
-#     [0,0,1,1,1,0,0],
-#     [0,1,1,1,1,1,0],
-#     [1,1,1,1,1,1,1],
-#     [0,0,0,1,0,0,0],
-#     [0,0,0,1,0,0,0],
-
-# ]
-# for image in picture:
-#     for pixel in image:
-#         if pixel:
-#             print('*',end='')
-#         else:
-#             print(' ',end='')
-#     print('')
+# # while True:
+# #     response = input('say something: ')
+# #     if(response=='bye'):
+# #         break
 
 
-some_list = ['a','b','c','b','d','m','n','n']
+# # picture = [
+# #     [0,0,0,1,0,0,0],
+# #     [0,0,1,1,1,0,0],
+# #     [0,1,1,1,1,1,0],
+# #     [1,1,1,1,1,1,1],
+# #     [0,0,0,1,0,0,0],
+# #     [0,0,0,1,0,0,0],
 
-def find_dup(a):
-    duplicate = []
-    for item in some_list:
-        if some_list.count(item) > 1 and item not in duplicate:
-            duplicate.append(item)
-    print (duplicate)
+# # ]
+# # for image in picture:
+# #     for pixel in image:
+# #         if pixel:
+# #             print('*',end='')
+# #         else:
+# #             print(' ',end='')
+# #     print('')
 
-find_dup(some_list)
+
+# # some_list = ['a','b','c','b','d','m','n','n']
+
+# # def find_dup(a):
+# #     duplicate = []
+# #     for item in some_list:
+# #         if some_list.count(item) > 1 and item not in duplicate:
+# #             duplicate.append(item)
+# #     print (duplicate)
+
+# # find_dup(some_list)
+
+# # def say_hello(name='default', id=0):
+# #     print(f'hello {name}, ID: {id}')
+
+# # say_hello("Henok", 25)
+# # say_hello("Tru", 26)
+# # say_hello(id = 75, name='John')
+# # say_hello()
+# # say_hello('Tony')
+
+# def sum(num1, num2):
+#     def another_func(n1,n2):
+#         return n1 + n2
+#     return another_func(num1, num2)
+
+# print(sum(12, 22))
+
+def test(a):
+    '''
+    Info: This function tests and prints param a
+    '''
+    print(a)
+
+help(test)
+print(test.__doc__)
