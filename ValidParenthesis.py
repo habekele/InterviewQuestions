f4c9ca130032a237fca3eb535207ddcda0d563b3
Henok Bekele <henokbekele1997@gmail.com>
2023-03-26 12:44:11 -0500

    wrote it different and added comments

--- ValidParenthesis.py
+++ ValidParenthesis.py
@@ -3,12 +3,13 @@ def isValid(s):
     stack = []
 
     for x in s:
-        if x not in map:
-            stack.append(x)
-            
-        if not stack or stack[-1] != map[x]:
-            return False
-        stack.pop()
-    return not stack
+        if x in map:# checking to see if x is a closed parenthesis
+            if stack and stack[-1]==map[x]:# if there is something is the stack and the top of the stack is the correct closing bracket
+                stack.pop()#removing the open bracket
+            else:
+                return False# returning false
+        else:
+            stack.append(x)#if an open bracket then add to the stack
+    return True if not stack else False #if there is still stuff in the stack then return false
 
-print(isValid('{[]}'))
+print(isValid('{[}'))
